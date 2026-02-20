cd8391d8f921cefa9eee51bde30d3dd60207bfbf
rhbollinger1s <rhbollinger1s@gmail.com>
2026-02-20 14:38:46 -0600

    add i3 module, rhbollinger1s config changes
    Signed-off-by: rhbollinger1s <rhbollinger1s@gmail.com>

---
+++ Modules/Desktop/i3.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, ... }: {
+# ----- [ i3 ] ------------------------------
+    imports = [ ];
+      environment.pathsToLink = [ "/libexec" ];
+  ...
+  services.xserver = {
+    enable = true;
+
+    desktopManager = {
+      xterm.enable = false;
+    };
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu
+        i3status
+     ];
+    };
+  };
+
+  programs.i3lock.enable = true;
+}
